3c9eb56edd193af9a146aa725172261f323132c7
Lucas <joselucasrs@gmail.com>
2018-11-12 01:52:50 -0200

    Modified until episode 10

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,10 @@ Route::get('/hello', function () {
 // });
 
 Route::get('/', 'PagesController@index');
-
 Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
 
+Route::resource('posts', 'PostsController');
+Auth::routes();
+
+Route::get('/dashboard', 'DashboardController@index');
